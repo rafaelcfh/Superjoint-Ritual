02fcbe9df51991a3528fb7c28342491233c1b93d
rafaelcfh <rafaelcfh@gmail.com>
2026-03-15 10:00:56 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'brewflow-v4';
+const CACHE = 'brewflow-v1';
 const ASSETS = ['index.html', 'style.css', 'app.js', 'recipes.js', 'manifest.json'];
 
 self.addEventListener('install', e => {
